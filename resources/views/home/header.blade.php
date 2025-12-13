3d42c81194dcafcece29e46d9de894f5946d3d9e
SadiaMabsurah <mabsurahsadia@gmail.com>
2025-12-13 19:36:44 +0600

    added login and register in the home page

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -36,6 +36,13 @@
                               <li class="nav-item">
                                  <a class="nav-link" href="contact.html">Contact Us</a>
                               </li>
+                                 <li class="nav-item">
+                                 <a class="nav-link" href="contact.html">Login</a>
+                              </li>
+                                <li class="nav-item">
+                                 <a class="nav-link" href="contact.html">Register</a>
+                              </li>
+                              
                            </ul>
                         </div>
                     </nav>
