224463f604ac60d58da67d9b7f7b272b3380d182
SadiaMabsurah <mabsurahsadia@gmail.com>
2025-12-14 10:33:19 +0600

    Add route for create_room page in AdminController

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,4 @@ route::get('/',[AdminController::class,'home']);
 route::get('/home',[AdminController::class,'index'])->name('home');
 
 
+route::get('/create_room',[AdminController::class,'create_room']);
