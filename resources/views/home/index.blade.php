26ab5abde9227ca5cf3f4fe13d9d77bf97751117
SadiaMabsurah <mabsurahsadia@gmail.com>
2025-12-14 18:41:14 +0600

    Remove blog section from home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -28,9 +28,6 @@
       <!-- gallery -->
       @include('home.gallery')
       <!-- end gallery -->
-      <!-- blog -->
-      @include('home.blog')
-      <!-- end blog -->
       <!--  contact -->
        @include('home.contact')
       <!-- end contact -->
