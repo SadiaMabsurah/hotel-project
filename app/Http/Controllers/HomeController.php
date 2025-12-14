c92514ae90e714f874b2e2f2093f5e6937069a96
SadiaMabsurah <mabsurahsadia@gmail.com>
2025-12-14 18:33:12 +0600

    Add book_room method to store room booking with validation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,15 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\Room;
+use App\Models\Booking;
 
 class HomeController extends Controller
 {
     public function room_details($id)
     {
         $room = Room::find($id);
+        return view('home.room_details', compact('room'));
+    }
 
-        return view('home.room_details',compact('room'));
+    public function book_room(Request $request, $id)
+
+    {
+        $request->validate([
+            'startDate'=>'required|date',
+            'endDate'=>'date|after:startDate',
+        ]);
+        $booking = new Booking();
+
+        $booking->room_id = $id;
+        $booking->name = $request->name;
+        $booking->email = $request->email;
+        $booking->phone = $request->phone;
+        $booking->start_date = $request->startDate;
+        $booking->end_date = $request->endDate;
+
+        $booking->save();
+
+        return redirect()->back()->with('success', 'Room booked successfully!');
     }
 }
