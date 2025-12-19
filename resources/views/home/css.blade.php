8c4d83f36d521cb0de0656c623fd8e2f675064fb
SadiaMabsurah <mabsurahsadia@gmail.com>
2025-12-20 01:42:57 +0600

    Add style for logo image

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -1,27 +1,49 @@
 <!-- basic -->
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
 <!-- mobile metas -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+
 <!-- site metas -->
-<title>keto</title>
+<title>SaFa</title>
 <meta name="keywords" content="">
 <meta name="description" content="">
 <meta name="author" content="">
+
 <!-- bootstrap css -->
 <link rel="stylesheet" href="css/bootstrap.min.css">
+
 <!-- style css -->
 <link rel="stylesheet" href="css/style.css">
-<!-- Responsive-->
+
+<!-- Responsive -->
 <link rel="stylesheet" href="css/responsive.css">
+
 <!-- fevicon -->
 <link rel="icon" href="images/fevicon.png" type="image/gif" />
+
 <!-- Scrollbar Custom CSS -->
 <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-<!-- Tweaks for older IEs-->
+
+<!-- Font Awesome -->
 <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+<!-- Fancybox -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+
+<!-- LOGO SIZE STYLE -->
+<style>
+    .logo-img {
+        width: 180px;
+        height: auto;
+        margin-top: -60px;   
+    }
+</style>
+
+
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
