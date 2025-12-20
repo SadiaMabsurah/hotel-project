2935c5cbea614c198ac55221f32b2ac65feec0ef
Fawziatahia <145826661+Fawziatahia@users.noreply.github.com>
2025-12-20 15:05:12 +0600

    Show user messages in admin panel
    Allows admins to view messages sent by users.

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,5 @@ Route::post('/upload_gallary', [AdminController::class, 'upload_gallary']);
 Route::get('/delete_gallary/{id}', [AdminController::class, 'delete_gallary']);
 
 Route::post('/contact', [HomeController::class, 'contact']);
+
+Route::get('/all_messages', [AdminController::class, 'all_messages']);
