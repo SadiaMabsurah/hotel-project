851eb614009425a59c6a84586bd2ad17e62336ae
SadiaMabsurah <mabsurahsadia@gmail.com>
2025-12-20 13:34:56 +0600

    Change homepage about us description

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -4,7 +4,7 @@
                <div class="col-md-5">
                   <div class="titlepage">
                      <h2>About Us</h2>
-                     <p>The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum. </p>
+                     <p>Welcome to SaFa Hotel, where comfort meets elegance. Our hotel is dedicated to providing guests with a memorable stay through exceptional service, cozy rooms, and modern amenities. Whether you are here for business or leisure, our friendly staff ensures a relaxing and enjoyable experience. At SaFa Hotel, we strive to create a warm and inviting atmosphere, making every visit special. </p>
                      <a class="read_more" href="Javascript:void(0)"> Read More</a>
                   </div>
                </div>
